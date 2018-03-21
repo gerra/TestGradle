d1ad4d1d7a5b1671f526b51115624fb13fcc22c1
gerra <gerralizza@gmail.com>
2018-03-22 02:08:55 +0300

    Make me understand I don't know java compile process

--- src/main/java/Implementation.java
+++ src/main/java/Implementation.java
@@ -4,18 +4,13 @@ public class Implementation extends AbstractClass {
         System.out.println("Implementation#foo");
     }
 
-    @Override
-    public void bar() {
-        System.out.println("Implementation#bar");
-    }
-
     public static void main(String[] args) {
         Implementation implementation = new Implementation();
         implementation.foo();
         implementation.bar();
-
-        // Описание для compileOnly :interface и implementation :abstractclass (в котором implementation :interface)
-        Interface _interface = new Implementation();
-        _interface.bar();
+//
+//        // Описание для compileOnly :interface и implementation :abstractclass (в котором implementation :interface)
+//        Interface _interface = new Implementation();
+//        _interface.bar();
     }
 }
