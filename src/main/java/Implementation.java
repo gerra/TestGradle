5f580f7a01218801e1ef88f10d6e6be02b3ac2c9
gerra <gerralizza@gmail.com>
2018-03-22 01:47:46 +0300

    Remove my stupid

--- src/main/java/Implementation.java
+++ src/main/java/Implementation.java
@@ -15,10 +15,7 @@ public class Implementation extends AbstractClass {
         implementation.bar();
 
         // Описание для compileOnly :interface и implementation :abstractclass (в котором implementation :interface)
-        // Нам пофиг на объявление Interface, потому что type erasure
         Interface _interface = new Implementation();
         _interface.bar();
-        // Но не пофиг на обращение к методам интерфейса, потому что они рантаймовские
-//        _interface.foo();
     }
 }
